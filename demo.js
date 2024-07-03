74b52bc19325b75e3917368c641258a404574506
Varshini-frontend <varshinianandaraj@gmail.com>
2024-07-03 05:53:26 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+let arr = ['Orange',25,100,true,33.58];
+console.log(arr);
+
+let val = [25, false, "green", "apple"];
+console.log(val[0]);
+
+let values =   [25, 50, "center", 75, 100];
+console.log(values.splice(0,1,5));
+console.log(values);
+
+let lenArr =  [12,23,"KGM",45,56,67];
+let arrLength = lenArr.length;
+console.log(arrLength);
+
+let ins = [1,2,4];
+let newElement = 100;
+ins.push(newElement);
+console.log(ins);
+
+let del = [1,2,3,4,"KGM","empty"];
+console.log(del.pop());
+console.log(del);
+
